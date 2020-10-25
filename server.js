a68fe7e4de1b2f2f5922e7ec4bfd18fc1e73eb94
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-10-25 12:59:22 -0700

    deployment steps

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mohamedsalihdev:Mmasm&a4f6d5!@cluster0.dd1wh.mongodb.net/workout?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on Port" + PORT);
+});
